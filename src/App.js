70e28a2bf800fe87a2c381f5c10b64559605db50
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-13 15:27:37 -0300

    create services component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import Site from './Site';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Top from './components/Top';
+import About from './components/About';
+import Services from './components/Services';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Top />
+        <About />
+        <Services />
         <Site />
         <Footer />
       </div>
