a1b41884e307aa9e0df03b96937d9ca59a94bcd5
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-13 17:28:26 -0300

    encapsulates all components with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+//BrowserRouter encapsulates all routes and Route creates routes
 
-import Site from './Site';
-import Footer from './components/Footer';
 import Header from './components/Header';
 import About from './components/About';
 import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <About />
-        <Services />
-        <Site />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <About />
+          <Services />
+          <Portfolio />
+          <Pricing />
+          <Contact />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
