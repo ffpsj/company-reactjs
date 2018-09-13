92534b327c83d2b9477d28e72e7b8db2e06c82e2
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-13 14:27:07 -0300

    create header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Site from './Site';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Site />
         <Footer />
       </div>
