8d59e43ce5d55d3b89e8166334ac89a77fa8816c
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-13 15:00:48 -0300

    create top component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Site from './Site';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Top from './components/Top';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Top />
         <Site />
         <Footer />
       </div>
