83306f279c164365a586d17f8d4ad0991ab2e38d
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-14 15:41:06 -0300

    add function to top button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ import React from 'react';
 const Footer = props => {
     return(
         <footer className='container-fluid text-center'>
-            <a href='#myPage' title='To Top'>
-            <span className='glyphicon glyphicon-chevron-up'></span>
-            </a>
+            <button className='btn btn-link'>
+                <span className='glyphicon glyphicon-chevron-up' onClick={() => window.scrollTo(0, 0)}></span>
+            </button>
             <p>Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
         </footer>
     )
