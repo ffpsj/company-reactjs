71e50cf65bf825345fa49cf831b4a3adb8cb14b0
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-18 17:50:09 -0300

    add auth proccess

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import config, { storage, auth } from './../firebase-config';
 
 class Login extends Component {
     constructor(props) {
         super(props)
+
+        //initial state
+        this.state = {
+            isAuthenticated: false,
+            isLoging: false,
+            error: false,
+        }
 
         //initiates login and password as null
         this.loginEmail = null
@@ -16,17 +24,40 @@ class Login extends Component {
     authUser() {
         console.log(this.loginEmail.value, this.loginPassword.value)
 
+        //current state, after button has clicked
+        this.setState({
+            isAuthenticated: false,
+            isLoging: true,
+            error: false,
+        })
+
         //authenticate what comes from the form
         //if the user is authenticated, show it, otherwise show the error
         auth.signInWithEmailAndPassword(this.loginEmail.value, this.loginPassword.value).then(user => {
             console.log('Usuário: ', user)
+            this.setState({
+                isAuthenticated: true,
+                isLoging: false,
+                error: false,
+            })
         })
         .catch(err => {
             console.log('Erro: ', err)
+            this.setState({
+                isAuthenticated: false,
+                isLoging: false,
+                error: true,
+            })
         })
     }
 
     render() {
+        {/* if the user is authenticated, redirect to the "admin" */}
+        if(this.state.isAuthenticated) {
+            return(
+                <Redirect to='/admin' />
+            )
+        }
         return(
             <div className='administrativeArea'>
                 <h2 className='margin-top-h2 text-center'>Login</h2>
@@ -35,15 +66,17 @@ class Login extends Component {
                 <div className='form-group'>
                     <label htmlFor='loginEmail'>Email Address</label>
                     <input type='email' className='form-control' id='loginEmail' aria-describedby='emailHelp' placeholder='Enter Email' ref={(ref) => this.loginEmail = ref} />
-                    <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
                 </div>
                 <div className='form-group'>
                     <label htmlFor='loginPassword'>Password</label>
                     <input type='password' className='form-control' id='loginPassword' placeholder='Enter Password' ref={(ref) => this.loginPassword = ref} />
+                    {/* is shown only if there is an error */}
+                    {this.state.error && <small id='emailHelp' className='form-text text-muted error-text'>Invalid e-mail or password</small>}
                 </div>
                 {/* change button type from submit to button */}
                 {/* create onClick method */}
-                <button type='button' className='btn btn-block btn-lg' onClick={this.authUser}>Submit</button>
+                {/* if isLogging true disable button */}
+                <button type='button' disabled={this.state.isLoging} className='btn btn-block btn-lg' onClick={this.authUser}>Submit</button>
                 {/*</form>*/}
             </div>
         )
